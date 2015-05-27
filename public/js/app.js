71f939f842afe2a58b92beb14e66d791a0369bcf
Magda Bonczkowska <magda1231@gmail.com>
2015-05-27 08:02:09 +0200

    Redirect

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('MyApp', ['ngRoute']).
+    config(['$routeProvider', function($routeProvider) {
+$routeProvider.when('/login', {
+		templateUrl: 'login.html',
+		controller: 'LoginController'
+	});
+	
+	$routeProvider.otherwise({ redirectTo: '/login' });
+}]);
+
+app.controller('LoginController', function(){
+});
